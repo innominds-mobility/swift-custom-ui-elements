beec56538e1fa13ecef6c5c2e520f036e7c63f64
Deepthi Muramshetty <dmuramshetty@innominds.com>
2017-05-23 15:01:17 +0530

    Code documented for UIImageview circle & square

--- swift-custom-ui/swift-custom-ui/ImageShapesViewController.swift
+++ swift-custom-ui/swift-custom-ui/ImageShapesViewController.swift
@@ -9,14 +9,21 @@
 import UIKit
 import InnoUI
 
+/// The purpose of this view controller is to provide a user interface for UIImageView as circular and UIImage as Square.
+/// Go to Interface Builder for details.
+/// The `ImageShapesViewController` class is a subclass of the `UIViewController`.
 class ImageShapesViewController: UIViewController {
-
+    // MARK: - IBOutlet Properties
+    /// The imageview for the Circular image.
     @IBOutlet weak var circularImage: UIImageView!
+    /// The imageview for square image
     @IBOutlet weak var squareImge: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         circularImage.cropImageCircular()
+        /// Profile Image reference
         let profilImg = UIImage(named: "profileImg")
+        /// UIImage with square shape
         let sqrImg = profilImg?.cropImageSquared()
         squareImge.image = sqrImg
         // Do any additional setup after loading the view.
