52ef4c812ec372320119163d1bd05815587cbfb6
Deepthi <deepthi.muramshetty@gmail.com>
2017-06-12 17:19:04 +0530

    Code commented for loading indicator.

--- swift-custom-ui/InnoUI/UIViewController+InnoLoadingIndicator.swift
+++ swift-custom-ui/InnoUI/UIViewController+InnoLoadingIndicator.swift
@@ -8,21 +8,30 @@
 
 import Foundation
 
+// MARK: - UIViewController extension for animating loading indicator.
 extension UIViewController {
+    /// Loading indicator tag.
     var innoLoadingIndicatorTag: Int { return 999990 }
+    /// Loading view tag.
     var innoViewTag: Int { return 999991}
+    /// Start of loading indicator.
+    ///
+    /// - Parameters:
+    ///   - style: UIActivityIndicatorViewStyle.
+    ///   - location: location for indicator.
     public func startLoadingIndicator(
         style: UIActivityIndicatorViewStyle = .gray,
         location: CGPoint? = nil) {
         //Set the position - defaults to `center` if no`location`
-        
         //argument is provided
         
+        /// Loaction
         let loc = location ?? self.view.center
         //Ensure the UI is updated from the main thread
         //in case this method is called from a closure
         DispatchQueue.main.async(execute: {
             //Create the activity indicator
+            /// Loading indicator
             let loadingIndicator = UIActivityIndicatorView(activityIndicatorStyle: style)
             //Add the tag so we can find the view in order to remove it later
             loadingIndicator.tag = self.innoLoadingIndicatorTag
@@ -35,11 +44,13 @@ extension UIViewController {
         })
     }
     
+   /// Stop loading indicator.
    public func stopLoadingIndicator() {
         
         //Again, we need to ensure the UI is updated from the main thread!
         DispatchQueue.main.async(execute: {
             //Here we find the `UIActivityIndicatorView` and remove it from the view
+            /// Loading indicator for stoping.
             if let loadingIndicator = self.view.subviews.filter ({ $0.tag == self.innoLoadingIndicatorTag}).first
                 as? UIActivityIndicatorView {
                 loadingIndicator.stopAnimating()
@@ -48,13 +59,17 @@ extension UIViewController {
         })
     }
     
+    /// Start of loading indicator view.
+    ///
+    /// - Parameter loadTitle: Title for loading
     public func startLoadingView(loadTitle: String? = nil) {
         //Set the position - defaults to `center` if no`location`
         //argument is provided
+       /// ActivityIndicatorViewStyle.
        let style: UIActivityIndicatorViewStyle = .white
+        /// Location point.
         let loc = self.view.center
-//        let titl:String = "Loading..."
-        
+        /// Title name
         let titleName = (loadTitle ?? "").isEmpty ? "Loading..." : loadTitle!
         
         //Ensure the UI is updated from the main thread
@@ -62,25 +77,26 @@ extension UIViewController {
         DispatchQueue.main.async(execute: {
         
             //Create the activity indicator
+            /// Activity indicator
             let loadingIndicator = UIActivityIndicatorView(activityIndicatorStyle: style)
-            //Add the tag so we can find the view in order to remove it later
             loadingIndicator.tag = self.innoLoadingIndicatorTag
             //Set the location
             loadingIndicator.center = loc
             loadingIndicator.hidesWhenStopped = true
             //Start animating and add the view
             loadingIndicator.startAnimating()
+            /// Loading view
             let loadView = UIView()
             loadView.tag = self.innoViewTag
             loadView.frame = CGRect(x:50,
                                     y:self.view.center.y-40, width:self.view.frame.size.width-100, height:100)
             loadView.layer.masksToBounds = true
             loadView.layer.cornerRadius = 6
-            let blackCol = UIColor.black
-            loadView.backgroundColor = blackCol.withAlphaComponent(0.4)//UIColor.black
+            loadView.backgroundColor = UIColor.black.withAlphaComponent(0.4)
             self.view.addSubview(loadView)
             self.view.addSubview(loadingIndicator)
             self.view.bringSubview(toFront: loadingIndicator)
+            /// Title label.
             let titelLabel = UILabel()
             titelLabel.frame = CGRect(x:3, y:loadView.frame.size.height-40,
                                       width:loadView.frame.size.width-6, height:40)
@@ -91,6 +107,7 @@ extension UIViewController {
             loadView.addSubview(titelLabel)
         })
     }
+    /// Stop loading view.
     public func stopLoadingView() {
         //Again, we need to ensure the UI is updated from the main thread!
         DispatchQueue.main.async(execute: {
--- swift-custom-ui/swift-custom-ui/LoadingIndicatorViewController.swift
+++ swift-custom-ui/swift-custom-ui/LoadingIndicatorViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 import InnoUI
 
+/// The purpose of this view controller is to provide a user interface
+/// for showing loading/ activity indicator.
 class LoadingIndicatorViewController: UIViewController {
 
+    /// The button to start loading.
     @IBOutlet weak var startButtn: UIButton!
-    var isStart: Bool = true
-    var isStartView: Bool = true
+    /// The button to start loading view.
     @IBOutlet weak var loadViewButton: UIButton!
+    /// Bool for activity loading.
+    var isStart: Bool = true
+    /// Bool for loading view.
+    var isStartView: Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
         startButtn.setTitle("Start Loading", for: UIControlState.normal)
@@ -25,6 +31,9 @@ class LoadingIndicatorViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    /// It triggers start and stop for Loading indicator.
+    ///
+    /// - Parameter sender: The button that invokes this IBAction method.
     @IBAction func startStopLoadingButtonAction(_ sender: Any) {
         self.stopLoadingIndicator()
         self.stopLoadingView()
@@ -38,6 +47,9 @@ class LoadingIndicatorViewController: UIViewController {
             self.stopLoadingIndicator()
         }
     }
+    /// It triggers start and stop for Loading view.
+    ///
+    /// - Parameter sender: The button that invokes this IBAction method.
     @IBAction func startStopLoadingViewAction(_ sender: Any) {
         self.stopLoadingView()
         self.stopLoadingIndicator()
